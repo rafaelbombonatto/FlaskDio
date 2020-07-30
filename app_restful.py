be2a176e9ccb525fb1a6a3c20973f1efed164e9b
rafaelbombonatto <rafael.bombonatto@gmail.com>
2020-07-30 08:52:03 -0300

    persistência com sqlalchemy

--- app_restful.py
+++ app_restful.py
@@ -1,11 +1,97 @@
 from flask import Flask, request
 from flask_restful import Resource,Api
 from habilidades import Habilidades
+from models import Pessoas, Atividades
 import json
 
 
 app = Flask(__name__)
 api = Api(app)
+
+class Pessoa(Resource):
+   
+    def get(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        try:
+            response = {
+                'nome':pessoa.nome,
+                'idade': pessoa.idade,
+                'id': pessoa.id
+            }
+        except AttributeError:
+            response = {
+                'status':'error',
+                'mensagem':'pessoa não encontrada'
+            }    
+        return response
+    
+
+    def put(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        dados = request.json
+        if 'nome' in dados:
+            pessoa.nome = dados['nome']
+        if 'idade' in dados:
+            pessoa.idade = dados['idade']
+        pessoa.save()
+        response = {
+            'id':pessoa.id,
+            'nome': pessoa.nome,
+            'idade': pessoa.idade
+        }
+        return response
+
+    def delete(self, nome):
+        pessoa = Pessoas.query.filter_by(nome=nome).first()
+        pessoa.delete()
+        return {
+            'status':'sucesso',
+            'mensagem':'Pessoa excluida'
+        }
+
+class ListaPessoas(Resource):
+    def get(self):
+        pessoas = Pessoas.query.all()
+        response = [{'id':i.id, 'nome':i.nome} for i in pessoas]
+        return response
+
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas(nome=dados['nome'], idade=dados['idade'])
+        pessoa.save()
+        response = {
+            'id':pessoa.id,
+            'nome':pessoa.nome,
+            'idade':pessoa.idade
+        }
+        return response
+
+class ListaAtividades(Resource):
+    def get(self):
+        atividades = Atividades.query.all()
+        response = [{'id':i.id, 'atividade':i.nome, 'pessoa':i.pessoa.nome} for i in atividades]
+        return response
+
+    def post(self):
+        dados = request.json
+        pessoa = Pessoas.query.filter_by(nome=dados['pessoa']).first()
+        atividade = Atividades(nome=dados['nome'], pessoa=pessoa)
+        atividade.save()
+        response = {
+            'pessoa': atividade.pessoa.nome,
+            'nome': atividade.nome,
+            'id':atividade.id
+
+        }
+        return response
+
+
+api.add_resource(Pessoa,'/pessoa/<string:nome>/')        
+api.add_resource(ListaPessoas,'/pessoa/')  
+api.add_resource(ListaAtividades,'/atividade/')  
+
+
+
 
 desenvolvedores = [
     {
