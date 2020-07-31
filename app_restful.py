bc6e8f149b2b41ce477e5c2a20054644feb29c4a
rafaelbombonatto <rafael.bombonatto@gmail.com>
2020-07-31 08:37:07 -0300

    Aula com httpauth

--- app_restful.py
+++ app_restful.py
@@ -1,16 +1,30 @@
 from flask import Flask, request
 from flask_restful import Resource,Api
 from habilidades import Habilidades
-from models import Pessoas, Atividades
+from models import Pessoas, Atividades, Usuarios
+from flask_httpauth import HTTPBasicAuth
 import json
 
-
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+usuarios = {
+    'rafael':'123',
+    'joao':'321'
+}
+
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login,senha):
+        return False
+    #return usuarios.get(login) == senha    
+    return Usuarios.query.filter_by(login=login,senha=senha).first()
+
+
 class Pessoa(Resource):
-   
-    def get(self, nome):
+    @auth.login_required
+    def get(self, nome):        
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
             response = {
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas, db_session
+from models import Pessoas, db_session, Usuarios
 
 def insere_pessoa():
     pessoa = Pessoas(nome='João', idade=39)
@@ -19,8 +19,17 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='João').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_usuario():
+    usuario = Usuarios.query.all()
+    print(usuario)
+
+
 if __name__ == '__main__':
     #insere_pessoa()
     #consulta_pessoa()
-    exclui_pessoa()
-    consulta_pessoa()
+    insere_usuario('rafael','123')
+    consulta_usuario()
